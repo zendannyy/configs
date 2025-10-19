2899f6f5b79776b3b104727471112485451aff8e
Dan Zen <zendannyy@Dans-iMac.local>
2025-10-18 21:00:56 -0700

    For new laptops and installs

---
+++ os_check.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash 
+# script for new installs or docker hosts 
+
+# one-liner to determine os distro and id
+OS_ID=$(uname)
+
+if [ "$OS_ID" = 'Darwin' ]; then
+    echo $OS_ID
+    brew update
+    brew install git curl wget
+elif [$OS_ID = 'ubuntu' ]; then
+    echo $OS_ID
+    apt-get update -y 
+    apt-get install -y git curl wget
+else
+    echo "ERROR: Unsupported distribution: $OS_ID"
+
+fi 
